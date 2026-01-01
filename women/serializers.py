b78cb85b3201b567c82e5f03af112388c49dae50
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2026-01-01 12:31:13 +0500

    APIView

---
+++ women/serializers.py
@@ -0,0 +1,36 @@
+from .models import Women , Category
+from rest_framework import serializers
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=250)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update =serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(read_only=True)
+    cat_id = serializers.IntegerField()
+
+    def create(self,validater_data):
+        return Women.objects.create(**validater_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title',instance.title)
+        instance.content = validated_data.get('content',instance.content)
+        instance.time_update = validated_data.get('time_update',instance.time_update)
+        instance.is_published = validated_data.get('is_published',instance.is_published)
+        instance.cat_id = validated_data.get('cat_id',instance.cat_id)
+        instance.save()
+        return instance
+
+
+
+
+
+
+
+
+
+# class WomenSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Women
+#         fields = ('title','cat_id')
---
+++ women/views.py
@@ -0,0 +1,41 @@
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import render
+from .serializers import WomenSerializer
+from .models import Women
+from rest_framework import generics
+
+
+
+class WomenAPIView(APIView):
+    def get(self,request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self,request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post':serializer.data})
+
+    def put(self ,request ,*args , **kwargs):
+        pk = kwargs.get('pk',None)
+        if not pk :
+            return Response({'error':'hato'})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error':'hato'})
+
+        serializer = WomenSerializer(data=request.data , )
+
+
+
+
+
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
